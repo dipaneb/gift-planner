35668eca3979338615fd823c9719378c9078d7f2
dipaneb <dipaneb@users.noreply.github.com>
2026-02-19 23:19:50 +0100

    Added NuxtUI and tailwind css dependencies.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,16 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ui from "@nuxt/ui/vue-plugin";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia()) // Pinia initialized before Router because
-app.use(router)        // global navigation guard needs the store.
+app.use(createPinia()); // Pinia initialized before Router because
+app.use(router); // global navigation guard needs the store.
+app.use(ui);
 
-app.mount('#app')
+app.mount("#app");
