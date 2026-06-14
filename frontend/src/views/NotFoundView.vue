6493ce43e18b1a662d6dd7bd03cfebb7b1356d21
dipaneb <dipaneb@users.noreply.github.com>
2026-06-14 23:00:56 +0200

    Bumped every frontend dependencies to the latest.

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -81,9 +81,11 @@
 </template>
 
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+const { t } = useI18n();
 
 const goBack = () => {
   if (window.history.length > 1) {
